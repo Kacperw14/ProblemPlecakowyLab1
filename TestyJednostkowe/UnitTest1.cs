e1e591a67bb885fae75d0c42a5546d1d034daad2
Kacper Wojtysiak <259262@student.pwr.edu.pl>
2023-03-23 18:13:48 +0100

    Dodano Test

---
+++ TestyJednostkowe/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProblemPlecakowy;
+
+namespace TestyJednostkowe
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CzyZwracaJedenElementGdySieMiesciPrzynajmniejJeden()
+        {
+            int N = 10;
+            int seed = 1;
+            Plecak P = new Plecak(N, seed);
+            for (int capacity = 0; capacity < 20; capacity++)
+            {
+                P.PackBackpack(capacity); //sprawdzamy to
+                Item min = P.items.OrderBy(it => it.weight).ToList()[0];
+                if (capacity >= min.weight)
+                    Assert.IsTrue(min.x);
+            }
+        }
+
+        [TestMethod]
+        public void CzyZwracaPustyGdyNieMiesciSieNawetJeden()
+        {
+            int N = 10;
+            int seed = 1;
+            Plecak P = new Plecak(N, seed);
+
+            for (int capacity = 0; capacity < 20; capacity++)
+            {
+                P.PackBackpack(capacity); //sprawdzamy to
+                Item min = P.items.OrderBy(it => it.weight).ToList()[0];
+                if (capacity < min.weight)
+                    Assert.IsFalse(min.x);
+            }
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            int N = 10;
+            int seed = 1;
+            int capacity = 10;
+            Plecak P = new Plecak(N, seed);
+
+        }
+    }
+
+}
