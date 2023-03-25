517dd7cabf6f7b90427dae78877473828b2545ff
Kacper Wojtysiak <259262@student.pwr.edu.pl>
2023-03-25 16:54:19 +0100

    Pętle dla testów

--- TestyJednostkowe/UnitTest1.cs
+++ TestyJednostkowe/UnitTest1.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using ProblemPlecakowy;
+using System.Linq;
 
 namespace TestyJednostkowe
 {
@@ -12,58 +10,63 @@ namespace TestyJednostkowe
         [TestMethod]
         public void CzyPakujeJedenElementGdySieMiesciPrzynajmniejJeden()
         {
-            int N = 10;
-            int seed = 1;
-            //Plecak P = new Plecak(N, seed);
-            for (int capacity = 0; capacity < 20; capacity++)
+            int N = 100;
+            for (int seed = 0; seed < 10; seed++)
             {
-                Plecak P = new Plecak(N, seed, capacity);
-                P.PackBackpack(); //sprawdzamy to
-                Item min = P.items.OrderBy(it => it.weight).ToList()[0];
-                if (capacity >= min.weight)
-                    Assert.IsTrue(min.x);
+                for (int capacity = 0; capacity < 100; capacity += 10)
+                {
+                    Plecak P = new Plecak(N, seed, capacity);
+                    P.PackBackpack();
+                    Item min = P.items.OrderBy(it => it.weight).ToList()[0];
+                    if (capacity >= min.weight)
+                        Assert.IsTrue(min.x);
+                }
             }
         }
 
         [TestMethod]
         public void CzyZwracaPustyGdyNieMiesciSieNawetJeden()
         {
-            int N = 10;
-            int seed = 1;
-            //Plecak P = new Plecak(N, seed);
-
-            for (int capacity = 0; capacity < 20; capacity++)
+            int N = 100;
+            for (int seed = 0; seed < 10; seed++)
             {
-                Plecak P = new Plecak(N, seed, capacity);
-                P.PackBackpack(); //sprawdzamy to
-                Item min = P.items.OrderBy(it => it.weight).ToList()[0];
-                if (capacity < min.weight)
-                    Assert.IsFalse(min.x);
+                for (int capacity = 0; capacity < 100; capacity += 10)
+                {
+                    Plecak P = new Plecak(N, seed, capacity);
+                    P.PackBackpack();
+                    Item min = P.items.OrderBy(it => it.weight).ToList()[0];
+                    if (capacity < min.weight)
+                        Assert.IsFalse(min.x);
+                }
             }
         }
 
         [TestMethod]
         public void CzyTworzySieOdpowiedniaLiczbaPrzedmiotow()
         {
-            int N = 10;
-            int seed = 1;
-            for (int capacity = 0; capacity < 20; capacity++)
+            int N = 100;
+            for (int seed = 0; seed < 10; seed++)
             {
-                Plecak P = new Plecak(N, seed, capacity);
-                Assert.IsTrue(N == P.items.Count);
+                for (int capacity = 0; capacity < 100; capacity += 10)
+                {
+                    Plecak P = new Plecak(N, seed, capacity);
+                    Assert.IsTrue(N == P.items.Count);
+                }
             }
         }
 
         [TestMethod]
         public void CzyDobrzePakuje()
         {
-            int N = 10;
-            int seed = 1;
-            for (int capacity = 0; capacity < 20; capacity++)
+            int N = 100;
+            for (int seed = 0; seed < 10; seed++)
             {
-                Plecak P = new Plecak(N, seed, capacity);
-                P.PackBackpack();
-                Assert.IsTrue(capacity >= P.package);
+                for (int capacity = 0; capacity < 100; capacity += 10)
+                {
+                    Plecak P = new Plecak(N, seed, capacity);
+                    P.PackBackpack();
+                    Assert.IsTrue(capacity >= P.package);
+                }
             }
         }
     }
