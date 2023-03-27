d0eeeb38d06106a80960ff5ea7c30f757444ff74
akopij70 <a.kopij70@gmail.com>
2023-03-27 16:37:30 +0200

    Usuniecie starego kodu do konsoli

--- ProblemPlecakowy/Program.cs
+++ ProblemPlecakowy/Program.cs
@@ -12,23 +12,6 @@ namespace ProblemPlecakowy
         static void Main(string[] args)
         {
             Application.Run(new Form1());
-
-            /*
-             * Console.WriteLine("Podaj liczbe przedmiotow");
-            int n = int.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Podaj seed");
-            int seed = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Podaj pojemność plecaka");
-            int C = int.Parse(Console.ReadLine());
-
-            Plecak plecak = new Plecak(n, seed, C);
-            Console.WriteLine(plecak);
-            plecak.PackBackpack();
-            Console.WriteLine(plecak);
-            Console.ReadLine();
-            */
         }
     }
 }
